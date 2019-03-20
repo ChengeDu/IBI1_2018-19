c58c62efd885c9cfb284e79cf7031d29f7646293
Joanna <chenge.18@intl.zju.edu.cn>
2019-03-20 09:51:39 +0800

    improvements for practical 4

--- Practical4/powersof2.py
+++ Practical4/powersof2.py
@@ -5,8 +5,10 @@ Created on Wed Mar 13 09:22:27 2019
 @author: Joanna
 """
 
-n=1750
-print(n,'=',end="")
+a=input()
+n=int(a)
+print(n,' is ',end="")
+#another way: a=a+' is '
 if n==0:
     print('0')
 while n!=0:
@@ -23,4 +25,8 @@ while n!=0:
         print('2**',count-1,'+',end="")
     else:
         print('2**',count-1)
+    # another way:   if n!=0:
+    #                   a=a+'2**'+str(count-1)+'+'
+    #                else:
+    #                   a=a+'2**'+str(count-1)
     
