2788d060230b6ac58672a37559afec81cfbba081
Joanna <chenge.18@intl.zju.edu.cn>
2019-05-11 17:46:05 +0800

    Some improvements of Practical 6

--- Week6/Sending Emails!.py
+++ Week6/Sending Emails!.py
@@ -1,5 +1,12 @@
 # -*- coding: utf-8 -*-
 """
+Created on Sat May 11 16:11:31 2019
+
+@author: Joanna
+"""
+
+# -*- coding: utf-8 -*-
+"""
 Created on Tue Mar 26 18:08:39 2019
 
 @author: Joanna
@@ -11,49 +18,52 @@ from email.mime.text import MIMEText
 from email.header import Header
 sender='3180111437@zju.edu.cn'
 #receiver='1666148325@qq.com'
-# 第三方 SMTP 服务
-mail_host="smtp.zju.edu.cn"  #设置服务器
-mail_user="3180111437"    #用户名
-mail_pass="xxxxx"   #口令 
+# SMTP service
+mail_host="smtp.zju.edu.cn"  #server
+mail_user="3180111437"    #user
+mail_pass="xxxxx"   #password
 
-#xaddress=open('abcd.txt','r')
+#print the sender information
+print('From:',sender)
+print('Password:',mail_pass)
+
 xaddress=open('address_information.csv','r')
 con=open('body.txt','r')
 content=con.read()
 for line in xaddress:
     line=line.rstrip()
-    name=str(re.findall(r'^([a-zA-Z]+)',line))
-    leng=len(name)-2
-    name=name[2:leng]
-    #at first I forget that [::]cannot change the original 'name' sequence 
-    subject=str(re.findall(r'To.+',line))
-    leng1=len(subject)-2
-    subject=subject[2:leng1]
-    print(name,' ',end='')
+    #use split to devide the line into 3 parts
+    lineinfo=line.split(',')    
+    name=lineinfo[0]
+    receiver=lineinfo[1]
+    subject=lineinfo[2]
+
+    #if it is line 1, then skip the following procedure
     if name=='name':
         continue
+    #bo is used for record whether it is a correct address
     bo=False
-    if re.search(r'[^,]+@[0-9a-zA-Z]+.com',line):
-        #actually .com is not enough to check
-        receiver=re.findall(r'[^,]+@[0-9a-zA-Z]+.com',line)
-        print(receiver)
-        bo=True
-        print('Correct Address!')
-    elif re.search(r'[^,]+@[0-9a-zA-Z]+.[a-zA-Z]+.cn',line):
-        receiver=re.findall(r'[^,]+@[0-9a-zA-Z]+.[a-zA-Z]+.cn',line)
-        print(receiver)
-        bo=True
-        print('Correct Address!')
-    else:
-        print('Wrong Address!')
-    #a better way: re_mail=re.compile(r^[0-9A-Za-z_]+@[0-9A-Za-z_]+(\.[0-9A-Za-z_]+)+$')
-    #              re_loginname=re.compile(r'(\S+)@')
     
+    #judge whether it is a valid address
+    if re.search(r'[^,]+@[0-9a-zA-Z]+.com',receiver):
+        #actually .com is not enough to check
+        bo=True
+        print(receiver,end='')
+        print(': Correct Address!')
+    elif re.search(r'[^,]+@[0-9a-zA-Z]+.[a-zA-Z]+.cn',receiver):
+        bo=True
+        print(receiver,end='')
+        print(': Correct Address!')
+    else:
+        print(receiver,end='')
+        print(': Wrong Address!')
+
+    #if it is a correct address, then send emails:
     if bo==True:
        message=content.replace('User',name)
        msg=MIMEText(message,'plain','utf-8')
        msg['From']=Header('Joanna','utf-8')
-       msg['To']=Header('practical','utf-8')
+       msg['To']=Header(receiver,'ascii')
        msg['Subject']=Header(subject,'utf-8')        
        try:
            server=smtplib.SMTP(mail_host,25)
@@ -66,9 +76,3 @@ for line in xaddress:
            print('Mail sent failed!')
 
         
-#a better solution from my classmates:
-#use .split to divide a line from address_information.csv to 3 line
-#that means: ['Mary']
-#            ['ibidiletest2@163.com']
-#            ['To:...']
-#it is easy to find the 3 things I want          
